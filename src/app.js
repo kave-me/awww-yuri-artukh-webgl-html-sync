5911906359f8cb73cb2f3d18ea320578dd076909
Kave McDaniel <kave.mdi@gmail.com>
2025-03-01 20:38:45 +0100

    lesson9 - add loading controlls - promise.all

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import * as THREE from 'three';
-import './style.css';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+// import './style.css';
+import { DigitalGlitch, OrbitControls } from 'three/examples/jsm/Addons.js';
 // @ts-ignore
 import fragmentShader from './shaders/fragment.glsl';
 // @ts-ignore
 import vertexShader from './shaders/vertex.glsl';
+import FontFaceObserver from 'fontfaceobserver';
+import imagesLoaded from 'imagesloaded';
+import Scroll from './scroll';
 
 export default class Sketch {
     constructor(options) {
@@ -18,12 +21,23 @@ export default class Sketch {
         this.camera = new THREE.PerspectiveCamera(
             70,
             this.width / this.height,
-            0.01,
-            10
+            100,
+            2000
         );
-        this.camera.position.z = 1;
 
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        const DISTANCE_TO_PLAIN = 600;
+        this.camera.position.z = DISTANCE_TO_PLAIN;
+
+        const cameraFov =
+            2 *
+            Math.atan(this.height / 2 / DISTANCE_TO_PLAIN) *
+            (180 / Math.PI);
+        this.camera.fov = cameraFov;
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true,
+        });
         this.container.appendChild(this.renderer.domElement);
 
         this.controls = new OrbitControls(
@@ -31,10 +45,104 @@ export default class Sketch {
             this.renderer.domElement
         );
 
-        this.resize();
-        this.setupResize();
-        this.addObject();
-        this.render();
+        this.images = [...document.querySelectorAll('img')];
+
+        const fontOpen = new Promise((resolve) => {
+            // @ts-ignore
+            new FontFaceObserver('Open Sans').load().then(() => resolve());
+        });
+        const fontPlayfair = new Promise((resolve) => {
+            // @ts-ignore
+            new FontFaceObserver('Playfair Display')
+                .load()
+                // @ts-ignore
+                .then(() => resolve());
+        });
+
+        const preloadImages = new Promise((resolve, reject) => {
+            imagesLoaded(
+                document.querySelectorAll('img'),
+                { background: true },
+                resolve
+            );
+        });
+
+        const allDone = [fontOpen, fontPlayfair, preloadImages];
+        this.currentScroll = 0;
+
+
+        Promise.all(allDone).then(() => {
+            this.scroll = new Scroll();
+            this.addImages();
+            this.resize();
+            this.setPositions();
+            this.setupResize();
+            this.addObject();
+            this.render();
+            window.addEventListener('scroll', (t) => {
+                this.currentScroll = window.scrollY;
+                this.setPositions();
+            });
+        });
+    }
+    addImages() {
+        this.imageStore = this.images.map((img) => {
+            let bounds = img.getBoundingClientRect();
+            let geometry = new THREE.PlaneGeometry(
+                bounds.width,
+                bounds.height,
+                1,
+                1
+            );
+            let texture = new THREE.Texture(img);
+            texture.needsUpdate = true;
+            let material = new THREE.MeshBasicMaterial({
+                // color: 0xff0000,
+                map: texture,
+            });
+            let mesh = new THREE.Mesh(geometry, material);
+
+            this.scene.add(mesh);
+            return {
+                img: img,
+                mesh: mesh,
+                top: bounds.top,
+                left: bounds.left,
+                width: bounds.width,
+                height: bounds.height,
+            };
+        });
+    }
+    updateImageStore() {
+        this.imageStore = this.images.map((img) => {
+            let bounds = img.getBoundingClientRect();
+            let mesh = this.imageStore.find((o) => o.img === img).mesh;
+
+            // Update geometry to match the new size
+            mesh.geometry.dispose();
+            mesh.geometry = new THREE.PlaneGeometry(
+                bounds.width,
+                bounds.height,
+                1,
+                1
+            );
+
+            return {
+                img: img,
+                mesh: mesh,
+                top: bounds.top + window.scrollY,
+                left: bounds.left,
+                width: bounds.width,
+                height: bounds.height,
+            };
+        });
+    }
+    setPositions() {
+        this.imageStore?.forEach((o) => {
+            o.mesh.position.y =
+                this.currentScroll - o.top + this.height / 2 - o.height / 2;
+            o.mesh.position.x = o.left - this.width / 2 + o.width / 2;
+        });
     }
     setupResize() {
         window.addEventListener('resize', this.resize.bind(this));
@@ -46,9 +154,12 @@ export default class Sketch {
         this.renderer.setSize(this.width, this.height);
         this.camera.aspect = this.width / this.height;
         this.camera.updateProjectionMatrix();
+        this.updateImageStore(); // Update image store with new positions and dimensions
+        this.setPositions(); // Update positions after resizing
+        console.log('resize');
     }
     addObject() {
-        this.geometry = new THREE.PlaneGeometry(0.5, 0.5, 20, 20);
+        this.geometry = new THREE.PlaneGeometry(200, 400, 10, 10);
         this.material = new THREE.ShaderMaterial({
             fragmentShader: fragmentShader,
             vertexShader: vertexShader,
@@ -60,14 +171,16 @@ export default class Sketch {
         });
 
         this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.scene.add(this.mesh);
+        // this.scene.add(this.mesh);
     }
 
     render() {
+        this.scroll.render();
+        this.currentScroll = this.scroll.scrollToRender;
+        this.setPositions();
         this.time += 0.05;
         // @ts-ignore
         this.material.uniforms.time.value = this.time;
-
 
         this.renderer.render(this.scene, this.camera);
         window.requestAnimationFrame(this.render.bind(this));
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
     plugins: [glsl()],
+    server: {
+        port: 3000,
+    }
 });
